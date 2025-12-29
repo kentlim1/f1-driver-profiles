68dc20fb55464f687b514de526a7315fa861fa57
Kent <171098648+kentlim1@users.noreply.github.com>
2025-12-29 14:24:31 -0800

    supabase auth stuff

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { supabase } from "./supabaseClient";
+
+export async function signUp(email: string, password: string) {
+  return await supabase.auth.signUp({ email, password });
+}
+
+export async function signIn(email: string, password: string) {
+  return await supabase.auth.signInWithPassword({ email, password });
+}
+
+export async function signOut() {
+  return await supabase.auth.signOut();
+}
